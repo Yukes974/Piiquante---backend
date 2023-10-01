c73bdd9d65bbb1dd8bbe0eb910c3cba60fd3d1e7
Patrick CHARLETTE <charlettepatrick@gmail.com>
2023-10-01 11:25:54 +0400

    Mis à jour d'un objet dans "Sauce"
    "...req.body, _id: req.params.id" permet de récupérer les nouvelles informations dans le corps de la requête et mettre à jour l'objet dont l'id est identique au paramètre ":id" dans "/api/sauces/;id".

--- app.js
+++ app.js
@@ -40,9 +40,15 @@ app.post("/api/sauces", (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 });
 
+app.put("/api/sauces/:id", (req, res, next) => {
+  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
 app.get("/api/sauces/:id", (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
-    .then((sauce) => res.status(200).json(thing))
+    .then((sauce) => res.status(200).json(sauce))
     .catch((error) => res.status(404).json({ error }));
 });
 
