90dc53760afd7416ed30a3d8ce9bfd0099cc9d7a
Patrick CHARLETTE <charlettepatrick@gmail.com>
2023-10-12 16:26:29 +0400

    Modification du "controller" "modifySauce" ainsi que sa route
    "sauceObject" est créé pour tester si "req.file" existe ou non.
    - Si c'est le cas, on traite l'image.
    - Sinon, on traite l'objet entrant.
    "...JSON.parse(req.body.sauce)" sert à récupérer l'objet en "parsant" la chaine de caractères.
    "imageUrl:`${req.protocol}://${req.get("host")}/images/${req.file.filename }` : {...req.body}" sert à recréer l'url de l'image créé précédemment et s'il n'y a pas d'objet de transmis, de récupérer l'objet directement dans le corps de la requête.

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -27,9 +27,32 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
-  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
+  const sauceObject = req.file
+    ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : { ...req.body };
+
+  delete sauceObject._userId;
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      if (sauce.userId != req.auth.userId) {
+        res.status(401).json({ message: "Non-autorisé" });
+      } else {
+        Sauce.updateOne(
+          { _id: req.params.id },
+          { ...sauceObject, _id: req.params.id }
+        )
+          .then(() => res.status(200).json({ message: "Objet modifié!" }))
+          .catch((error) => res.status(401).json({ error }));
+      }
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
 };
 
 exports.deleteSauce = (req, res, next) => {
--- routes/sauces.js
+++ routes/sauces.js
@@ -7,7 +7,7 @@ const multer = require("../middleware/multer-config");
 const saucesCtrl = require("../controllers/sauces");
 
 router.post("/", auth, multer, saucesCtrl.createSauce);
-router.put("/:id", auth, saucesCtrl.modifySauce);
+router.put("/:id", auth, multer, saucesCtrl.modifySauce);
 router.delete("/:id", auth, saucesCtrl.deleteSauce);
 router.get("/:id", auth, saucesCtrl.getSauceById);
 router.get("/", auth, saucesCtrl.getSauces);
